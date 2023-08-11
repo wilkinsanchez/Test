20dee27c94f5b501930370692e0a148306639534
Wilkin Sanchez <40305588+wilkinsanchez@users.noreply.github.com>
2023-08-11 00:25:56 -0400

    Update install_script.ps1

--- install_script.ps1
+++ install_script.ps1
@@ -1,1 +1,14 @@
-test
+# GitHub repository details
+$repoOwner = "wilkinsanchez"
+$repoName = "Test"
+$filePath = "install_script.ps1" # Path to the file you want to download
+
+# GitHub raw content URL
+$rawUrl = "https://raw.githubusercontent.com/$repoOwner/$repoName/main/$filePath"
+
+# Destination path to save the downloaded file
+$destinationPath = "C:\Users\adminuser\Desktop"
+
+# Download the file from GitHub
+Invoke-WebRequest -Uri $rawUrl -OutFile $destinationPath
+
